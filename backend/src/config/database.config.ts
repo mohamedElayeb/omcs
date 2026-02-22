0ca516f2ca3f2bd61c9e28de7eac110db3cb937a
mohamedElayeb <mohamedelayeb14@gmail.com>
2026-02-22 18:06:33 +0300

    Update database.config.ts

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,24 +1,15 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        url: config.get<string>('DATABASE_URL'),
-        autoLoadEntities: true,
-        synchronize: true,
-        ssl: config.get<string>('NODE_ENV') === 'production'
-          ? { rejectUnauthorized: false }
-          : false,
-      }),
-    }),
-  ],
-})
-export class AppModule {}
+export const getDatabaseConfig = (
+  config: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  url: config.get<string>('DATABASE_URL'),
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl:
+    config.get<string>('NODE_ENV') === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+});
